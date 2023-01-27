d9b32c37d190f75a2da8a9ff87b89bdc6a399b9b
Manuel Alba <malba@mmae.cl>
2023-01-27 17:53:20 -0300

    Fix traces

---
+++ pages/api/regions.js
@@ -0,0 +1,18 @@
+import apm from 'elastic-apm-node'
+import { prisma } from '../../db'
+|
+export default async function handler(req, res) {
+
+    var span = apm.startSpan('call DB')
+
+
+    const regions = await prisma.regions.findMany({
+        include: {
+          airports: true, // Return all fields
+        },
+      })
+    res.status(200).json(regions)
+
+    if (span) span.end()
+
+}
