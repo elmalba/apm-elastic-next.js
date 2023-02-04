6b1a060d961674011c45132e42add66d443f315c
Isaac Silva <isaac.silva@gmail.com>
2023-02-04 12:10:33 -0300

    service name updated

--- rum.js
+++ rum.js
@@ -2,7 +2,7 @@ import { init as initApm } from '@elastic/apm-rum'
 const apm = initApm({
 
   // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
-  serviceName: 'JsConf Chile -Frontend',
+  serviceName: 'JsConf Chile - Frontend',
 
   // Set custom APM Server URL (default: http://localhost:8200)
   serverUrl: 'https://negrote.apm.us-central1.gcp.cloud.es.io:443',
